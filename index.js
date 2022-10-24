dc5c49119e357bf3e378abdd6a85f0ac85728a46
Abi <91515654+awhittle2@users.noreply.github.com>
2022-10-24 00:53:59 -0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 $(document).ready(function() {
     $("#javaDiv").css("display", "none");
     $("#luaDiv").css("display", "none");
-    $(".timelinePar").css("display", "none");
 
     $("#kittyDiv").click(function(elt) {
         if($("#javaDiv").css('display') == 'none') {
@@ -12,18 +11,4 @@ $(document).ready(function() {
             $("#luaDiv").css("display", "none");
         }
     })
-
-    $("#timeline-item").click(function(elt) {
-        var userItem = $(this).attr("id");
-        userPar = "#" + userItem + "p";
-
-        if($(userPar).css('display') == 'none') {
-            $(userPar).css('display', 'block');
-        } else {
-            $(userPar).css('display', 'none');
-        }
-
-        console.log(userPar);
-    })
-
 })
